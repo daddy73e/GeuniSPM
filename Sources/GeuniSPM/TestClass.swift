1999087f906d3f8503480920f485c67cb7df452d
ygsong <ygsong@incruit.com>
2021-09-03 16:42:21 +0900

    open class testClass

---
+++ Sources/GeuniSPM/TestClass.swift
@@ -0,0 +1,15 @@
+//
+//  TestClass.swift
+//  GeuniSPM
+//
+//  Created by Yeongeun Song on 2021/09/03.
+//
+
+import Foundation
+
+open class TestClass: NSObject {
+    var key = "0101"
+    private func testCall() {
+        print("hello world")
+    }
+}
